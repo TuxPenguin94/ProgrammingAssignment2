0fec4dec6cdc25947cefed42f1963a7e9ed8fa0f
TuxPenguin94 <fm17g@list.ru>
2015-05-24 23:23:58 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions create a matrix and cache the result of the solution
+## It helps save time at time-consuming computations
 
-## Write a short comment describing this function
+## The function creates a "special" matrix. Actually, it looks like a list with its own environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The function solves the matrix. If the determinant is equal to 0, it throws an error.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("...Getting cached data...")
+                return(s)
+        }
+        data <- x$get()
+	if (det(data) == 0)
+		stop("The determinant is equal to 0. Can't solve the Matrix.")
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
